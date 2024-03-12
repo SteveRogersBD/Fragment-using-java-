56e8a52fa164a55d2a0c8ef7a4134fdb00cb15b6
Aniruddha Biswas Atanu <aniruddhabiswas105@gmail.com>
2024-03-12 16:18:03 -0500

    Intent Added

--- app/src/main/java/com/example/practise/Fragments/FirstFragment.java
+++ app/src/main/java/com/example/practise/Fragments/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.practise.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,17 +8,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.practise.MainActivity2;
 import com.example.practise.R;
 
 public class FirstFragment extends Fragment {
 
-
+    TextView text;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_first, container, false);
+        text = view.findViewById(R.id.textView);
+
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), MainActivity2.class);
+                startActivity(intent);
+                //Since we don't know in which activity this fragment can be therefore, instead of
+                //using MainActivity.this we use getContext();
+            }
+        });
+
         return view;
     }
 }
